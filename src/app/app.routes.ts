95c9e5bad58b26be816cc03afa26f924ffbf65e0
carmel26 <nkeshipelagie@gmail.com>
2025-01-14 16:41:23 +0200

    register Component on 14Jan2025

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,10 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from '../pages/login.component';
 
 export const routes: Routes = [
-  {path: '/login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'registration', loadComponent: () => {
+    return import('../pages/registration.component').then(
+      (m) => m.RegistrationComponent
+    );
+  }},
 ];
