19cbdc4c8b429c28ca2b90a0966376cf563fbb3b
carmel26 <nkeshipelagie@gmail.com>
2025-01-14 16:17:38 +0200

    adding loginComponent in our route and adding ProvideHttpClient in app.config.ts on 14Jan2025

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,12 @@ import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } fr
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    importProvidersFrom(HttpClientModule),
+    provideHttpClient(),
   ]
 };
