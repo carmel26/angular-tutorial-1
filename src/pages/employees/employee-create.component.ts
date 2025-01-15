f9e17ee436ff93d49f3287df3ce4b013781126f1
carmel26 <nkeshipelagie@gmail.com>
2025-01-15 13:07:51 +0200

    creation of employee-create component on 15Jan 2025 at 1307

---
+++ src/pages/employees/employee-create.component.ts
@@ -0,0 +1,31 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-employee-create",
+  template: `
+    <div class = "container">
+      <form (ngSubmit)="onSubmit()">
+        <div class="form-group">
+          <label for="firstname">Name:</label>
+          <input type="text" id="firstname" name="firstname" required>
+        </div>
+        <div class="form-group">
+          <label for="lastname">Lastname:</label>
+          <input type="text" id="lastname" name="lastname" required>
+        </div>
+        <div class="form-group">
+          <label for="position">Position:</label>
+          <input type="text" id="position" name="position" required>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+    `,
+  standalone : true,
+})
+export class EmployeeCreateComponent {
+  constructor() {}
+  onSubmit() {
+    console.log("Form submitted Thanks");
+  }
+}
