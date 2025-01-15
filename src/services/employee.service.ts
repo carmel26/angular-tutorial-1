d2a5408c55de66542a0c9a32c6bd83605ce84b2d
carmel26 <nkeshipelagie@gmail.com>
2025-01-15 12:41:41 +0200

    adding static data in the EmployeeService with GetEmplyees Method 15Jan2025 at 1241

--- src/services/employee.service.ts
+++ src/services/employee.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from "@angular/core";
+import { of } from "rxjs";
 
 @Injectable()
-export class EmployeeService{}
+export class EmployeeService{
+  getEmployees(){
+    return of([
+      { name: 'John', lastname: 'Bien', position: 'Developer' },
+      { name: 'Jane', lastname: 'Smith', position: 'Manager' }
+
+    ]);
+  }
+}
